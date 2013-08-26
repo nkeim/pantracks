ae96ef5b9c7d723a433f7e75079748f72681af83
Nathan Keim <nkeim@seas.upenn.edu>
2013-08-25 20:36:26 -0400

    Added numpy import in xform.py

--- pantracks/xform.py
+++ pantracks/xform.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def dfcrop(df, xmin=-np.inf, xmax=np.inf, ymin=-np.inf, ymax=np.inf, xcol='x', ycol='y'):
     """Crop a particle tracks DataFrame"""
     return df[(df[xcol] > xmin) & (df[xcol] < xmax) & \
